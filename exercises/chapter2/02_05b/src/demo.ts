3b43d5c9d88317fd24154c4a5c2f95277c7d8fc7
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-01-15 21:44:37 -0300

    02 _05 done

--- exercises/chapter2/02_05b/src/demo.ts
+++ exercises/chapter2/02_05b/src/demo.ts
@@ -2,7 +2,20 @@ interface Contact {
     id: number;
     name: string;
 }
+interface UserContact <TExternalID> {
+    id: number;
+    name: string;
+    username: string
+    externalId: TExternalID
+    loadExternalID(): Task <TExternalID>
+}
 
-function clone(source) {
+function clone<T1, T2 extends T1> (source: T1 ): T2 {
     return Object.apply({}, source);
 }
+
+const a: Contact ={ id:123, name:'Homer Simpson'}
+const b = clone<Contact, UserContact>(a)
+
+const dateRange = { startDate: Date.now(), endDate:Date.now() }
+const dateRangeCopy = clone(dateRange)
