ba1dd1994639e3fa388cd82fa0a6647bafac249b
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-01-15 21:21:13 -0300

    02_03 and 04 done

--- exercises/chapter2/02_02b/src/demo.ts
+++ exercises/chapter2/02_02b/src/demo.ts
@@ -1,18 +1,30 @@
+type ContactName = string
+
 interface Contact extends Address{
   id: number
-  name: string
+  name: ContactName //alias type
   birthDate?: Date //optional chaining
+  status: ContactStatus
 }
+
+enum ContactStatus{
+  Active= "active",
+  Inactive = "inactive",
+  New = 'new'
+}
+
 interface Address{
-  line1: string
-  line2: string
-  province:string
-  region: string
-  postalCode: string
+  // line1: string
+  // line2: string
+  // province:string
+  // region: string
+  // postalCode: string
 }
+
 let primaryContact: Contact ={
   name: "james johnson",
   id:12345,
   // birthDate: '01-01-1980', // dont allow to have variables with other types being optional chaining too
-  birthDate: new Date('01-01-1980')
+  birthDate: new Date('01-01-1980'),
+  status: ContactStatus.Inactive
 }
