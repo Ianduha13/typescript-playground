f6ece87f5d894d09cffa33396636fa5789ce76a4
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-01-25 17:26:12 -0300

    01 and 02 done

--- Ex_Files_Learning_TypeScript/Exercise Files/Ch01/01_02/src/model.ts
+++ Ex_Files_Learning_TypeScript/Exercise Files/Ch01/01_02/src/model.ts
@@ -1,11 +1,17 @@
-let displayName = "Jess's standing desk";
-let inventoryType = "furniture";
-let trackingNumber = "FD123455";
-let createDate = new Date();
-let originalCost = 425;
+let displayName: string = "Jess's standing desk";
+let inventoryType: string = "furniture";
+let trackingNumber:string = "FD123455";
+let createDate:Date = new Date();
+let originalCost:number = 425;
 
-function getInventoryItem(trackingNumber) {
-
+function getInventoryItem(trackingNumber:string): {
+  displayName: string
+  inventoryType:string
+  trackingNumber: string
+  createDate: Date
+  originalCost: number
+} {
+  return null
 }
 
 function saveInventoryItem(item) {
